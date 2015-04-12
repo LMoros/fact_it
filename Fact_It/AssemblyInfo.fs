6fc497209391d5a4fdefb4b0b8a81e2724760b0f
LMoros <LMoros1930@yahoo.com>
2015-04-12 11:35:13 -0400

    third commit

---
+++ Fact_It/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module Fact_It.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("Fact_It")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("Luis")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Fact_It/Concept.fs
@@ -0,0 +1,36 @@
+﻿namespace Concept
+
+module FactExp =    
+    type Fact<'key,'a> = 
+        | Journaled of ('key * 'a) // Represents an event executed and logged
+        | MeantToBe of 'a // Represent the conclusion of the emminent need to execute an action and journal the fact
+        | NotMeantToBe // Represent the conclusion that there is not action to be executed
+
+    type FactBuilder<'key, 'a> 
+            ( gather : unit -> Option<('key * 'a)> , // This lambda retrieves the fact if it has already been computer 
+              letItBe : 'a -> 'a , // This lambda executes all side effects related to the Fact (i.e. writting to disk, DB, queues, Web API calls, FTP, etc...)
+              journal : 'a -> ('key * 'a) ) = // The last lambda journals the Fact.
+
+        // Checks to see if the Fact was already computed and Journled.  If so, the stored informatio is provided. 
+        // If not the computation is executed
+        member this.Delay f =
+            match gather() with
+            | Some  b -> Journaled b
+            | None -> f()
+
+        // Composes different Facts
+        member this.Bind ( m ,  f ) =
+            match m with 
+            | Journaled (key , a) -> f a
+            | MeantToBe a -> f a
+            | _ -> NotMeantToBe  
+
+        // Executes side effects and journals the fact 
+        member this.Return a = letItBe a |> journal |> Journaled 
+
+        // returns a Fac<'key,'a>, if Meant-To-Be executes side effects and journals the fact
+        member this.ReturnFrom m =
+            match  m with 
+            | Journaled _ -> m
+            | MeantToBe a -> this.Return a
+            | _ -> NotMeantToBe
